dc956a3f5d0b62f300a250c88e9c891fc339d888
Maya Marten <Maya@Sleepgeekz.com>
2026-04-02 11:47:16 -0400

    Add SSM PIM v18

---
+++ ssm-pim-vscode/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './ssm-pim-v18.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
